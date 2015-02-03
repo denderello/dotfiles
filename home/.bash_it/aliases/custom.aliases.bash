0bd16c6ac4804722ae5e38afbb7f044fc51d9058
Dennis Benkert <spinecrasher@googlemail.com>
2015-02-03 16:03:41 +0100

    added aliases for influxdb-cli and http

--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -1,3 +1,6 @@
 alias gd='git diff'
 
 alias gpo='git push origin $(git rev-parse --abbrev-ref HEAD)'
+
+alias influxdb-cli="docker run --rm -it denderello/docker-influxdb-cli"
+alias http='docker run -it --rm --net=host clue/httpie'
