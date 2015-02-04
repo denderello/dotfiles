3d3d4ed65de8f2a4c4f408299f97518726772a2d
Dennis Benkert <spinecrasher@googlemail.com>
2015-02-05 00:09:54 +0100

    restructured aliases

--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -4,3 +4,16 @@ alias gpo='git push origin $(git rev-parse --abbrev-ref HEAD)'
 
 alias influxdb-cli="docker run --rm -it denderello/docker-influxdb-cli"
 alias http='docker run -it --rm --net=host clue/httpie'
+
+alias l="ls -lah"
+
+alias go-gopath="cd $GOPATH/src"
+alias go-home="cd ~"
+
+go-code () {
+  cd `go list -f '{{.Dir}}' $1`
+}
+
+go-gs () {
+  go-code github.com/giantswarm/$1
+}
