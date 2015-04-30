e6bdcaf9ff2f34e40156e8e1344ed95b2ffb14ac
Dennis Benkert <spinecrasher@googlemail.com>
2015-04-30 15:16:33 +0200

    added wrk docker container alias

--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -2,8 +2,9 @@ alias gd='git diff'
 
 alias gpo='git push origin $(git rev-parse --abbrev-ref HEAD)'
 
-alias influxdb-cli="docker run --rm -it denderello/docker-influxdb-cli"
+alias influxdb-cli="docker run --rm -it --net=host denderello/docker-influxdb-cli"
 alias http='docker run -it --rm --net=host clue/httpie'
+alias wrk='docker run -it --rm --net=host -v `pwd`:/data williamyeh/wrk:4.0.1'
 
 alias l="ls -lah"
 
