987132c53e7acc8632cef678070addace8680849
Dennis Benkert <spinecrasher@googlemail.com>
2015-11-26 16:49:35 +0100

    added csysdig alias

--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -5,6 +5,7 @@ alias gpo='git push origin $(git rev-parse --abbrev-ref HEAD)'
 alias influxdb-cli="docker run --rm -it --net=host denderello/docker-influxdb-cli"
 alias http='docker run -it --rm --net=host clue/httpie'
 alias wrk='docker run -it --rm --net=host -v `pwd`:/data williamyeh/wrk:4.0.1'
+alias csysdig='docker run -it --rm --name=sysdig-$(uuidgen) --privileged -v /var/run/docker.sock:/host/var/run/docker.sock -v /dev:/host/dev -v /proc:/host/proc:ro -v /boot:/host/boot:ro -v /lib/modules:/host/lib/modules:ro -v /usr:/host/usr:ro sysdig/sysdig csysdig'
 
 alias l="ls -lah"
 
